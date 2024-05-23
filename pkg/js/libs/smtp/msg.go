8e44bef8ac6fcfef272185b120b81c891fbf55c4
Co5mos <cosmos2evo@gmail.com>
2024-05-23 17:46:07 +0800

    add smtp attachment

--- pkg/js/libs/smtp/msg.go
+++ pkg/js/libs/smtp/msg.go
@@ -3,6 +3,7 @@ package smtp
 import (
 	"bufio"
 	"bytes"
+	"encoding/base64"
 	"net/textproto"
 	"strings"
 )
@@ -16,12 +17,14 @@ type (
 	// message.From('xyz@projectdiscovery.io');
 	// ```
 	SMTPMessage struct {
-		from string
-		to   []string
-		sub  string
-		msg  []byte
-		user string
-		pass string
+		from       string
+		to         []string
+		sub        string
+		msg        []byte
+		user       string
+		pass       string
+		attachment string
+		attachData []byte
 	}
 )
 
@@ -95,15 +98,53 @@ func (s *SMTPMessage) Auth(username, password string) *SMTPMessage {
 // message.To('xyz2@projectdiscoveyr.io');
 // message.Subject('hello');
 // message.Body('hello');
+// message.Attachment('file.txt', 'hello');
 // log(message.String());
 // ```
 func (s *SMTPMessage) String() string {
 	var buff bytes.Buffer
 	tw := textproto.NewWriter(bufio.NewWriter(&buff))
+
 	_ = tw.PrintfLine("To: %s", strings.Join(s.to, ","))
 	if s.sub != "" {
 		_ = tw.PrintfLine("Subject: %s", s.sub)
 	}
-	_ = tw.PrintfLine("\r\n%s", s.msg)
+
+	_ = tw.PrintfLine("MIME-Version: 1.0")
+	if s.attachment != "" {
+		boundary := "my-boundary-12345"
+		_ = tw.PrintfLine("Content-Type: multipart/mixed; boundary=%s", boundary)
+		_ = tw.PrintfLine("\r\n--%s", boundary)
+		_ = tw.PrintfLine("Content-Type: text/plain; charset=\"utf-8\"")
+		_ = tw.PrintfLine("\r\n%s", s.msg)
+		_ = tw.PrintfLine("\r\n--%s", boundary)
+		_ = tw.PrintfLine("Content-Type: application/octet-stream; name=\"%s\"", s.attachment)
+		_ = tw.PrintfLine("Content-Transfer-Encoding: base64")
+		_ = tw.PrintfLine("Content-Disposition: attachment; filename=\"%s\"", s.attachment)
+		encoded := base64.StdEncoding.EncodeToString(s.attachData)
+		for i := 0; i < len(encoded); i += 76 {
+			end := i + 76
+			if end > len(encoded) {
+				end = len(encoded)
+			}
+			_ = tw.PrintfLine("%s", encoded[i:end])
+		}
+		_ = tw.PrintfLine("\r\n--%s--", boundary)
+	} else {
+		_ = tw.PrintfLine("\r\n%s", s.msg)
+	}
 	return buff.String()
+}
+
+// Attachment adds an attachment to the message
+// @example
+// ```javascript
+// const smtp = require('nuclei/smtp');
+// const message = new smtp.SMTPMessage();
+// message.Attachment('file.txt', 'hello');
+// ```
+func (s *SMTPMessage) Attachment(filename string, data []byte) *SMTPMessage {
+	s.attachment = filename
+	s.attachData = data
+	return s
 }
